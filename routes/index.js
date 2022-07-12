14146122deeffd568949a1e21071f6ad5dd91f17
Owais Athar <owaisathar2882@gmail.com>
2022-07-12 15:04:45 +0530

    initial

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/search:searchString', async (req, res) => {
-  let api = await axios(`GET https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.API_KEY}&query=${req.params.searchString}`)
-  console.log(api)
-  res.render('index', {title: 'Express'})
-})
+
 
 module.exports = router;
